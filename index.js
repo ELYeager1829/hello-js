8623877f09607e2d0d8ae416c435fac13dba5e87
Lungile <lungilethobejane8@gmail.com>
2026-01-16 23:40:48 +0200

    information update

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello, World!");
+const name = "Lungile ";
+const age = 23;
+const fplanguge = "Java";
+
+console.log("Hello there , my name " + name + ". I'm " + age + " years old and my favorite programming language is " + fplanguge + ".");
